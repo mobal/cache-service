11d8c0b5ca9d80e64a4fb86074928709c3840e41
Balázs Molnár <bmolnar0510@gmail.com>
2025-05-16 22:21:16 +0200

    fix: removed unnecessary asnyc calls

--- app/middlewares.py
+++ app/middlewares.py
@@ -1,27 +1,52 @@
+import uuid
+from contextvars import ContextVar
+
 from aws_lambda_powertools import Logger
 from fastapi import Request, Response, status
+from fastapi.responses import JSONResponse
 from starlette.middleware.base import (BaseHTTPMiddleware,
                                        RequestResponseEndpoint)
-from starlette.responses import JSONResponse
 from starlette.types import ASGIApp
 
 X_API_KEY = "X-Api-Key"
+X_CORRELATION_ID = "X-Correlation-ID"
+
+correlation_id: ContextVar[str] = ContextVar(X_CORRELATION_ID)
+logger = Logger(utc=True)
 
 
 class APIKeyMiddleware(BaseHTTPMiddleware):
     def __init__(self, app: ASGIApp, api_key: str):
         super().__init__(app)
-        self.__api_key = api_key
-        self.__logger = Logger(utc=True)
+        self._api_key = api_key
 
     async def dispatch(
         self, request: Request, call_next: RequestResponseEndpoint
     ) -> Response:
-        if request.headers.get("X-Api-Key") != self.__api_key:
+        if request.headers.get("X-Api-Key") != self._api_key:
             error_message = "Invalid or missing API key"
-            self.__logger.warning(error_message)
+            logger.warning(error_message)
             return JSONResponse(
                 content={"message": error_message},
                 status_code=status.HTTP_403_FORBIDDEN,
             )
         return await call_next(request)
+
+
+class CorrelationIdMiddleware(BaseHTTPMiddleware):
+    def __init__(self, app: ASGIApp):
+        super().__init__(app)
+
+    async def dispatch(
+        self, request: Request, call_next: RequestResponseEndpoint
+    ) -> Response:
+        correlation_id.set(
+            request.headers.get(X_CORRELATION_ID)
+            or request.scope.get("aws.context", {}).aws_request_id
+            if request.scope.get("aws.context")
+            else str(uuid.uuid4())
+        )
+        logger.set_correlation_id(correlation_id.get())
+        response = await call_next(request)
+        response.headers[X_CORRELATION_ID] = correlation_id.get()
+        return response
--- tests/unit/repository/test_cache_repository.py
+++ tests/unit/repository/test_cache_repository.py
@@ -5,19 +5,18 @@ import pytest
 from app.repositories import CacheRepository
 
 
-@pytest.mark.asyncio
 class TestCacheRepository:
     @pytest.fixture
     def cache_repository(self, initialize_cache_table) -> CacheRepository:
         return CacheRepository()
 
-    async def test_successfully_create_key_value(
+    def test_successfully_create_key_value(
         self, cache_repository: CacheRepository, cache_table, data: dict[str, Any]
     ):
         data["key"] = "test"
         data["value"] = "test"
 
-        await cache_repository.create_key_value(data)
+        cache_repository.create_key_value(data)
 
         response = cache_table.get_item(
             Key={"key": data["key"]},
@@ -31,14 +30,12 @@ class TestCacheRepository:
         assert data["created_at"] == item["created_at"]
         assert data["ttl"] == item["ttl"]
 
-    async def test_successfully_get_key_value_by_key(
+    def test_successfully_get_key_value_by_key(
         self, cache_repository: CacheRepository, data: dict[str, Any]
     ):
-        assert data == await cache_repository.get_key_value_by_key(data["key"])
+        assert data == cache_repository.get_key_value_by_key(data["key"])
 
-    async def test_fail_to_get_key_value_by_key(
-        self, cache_repository: CacheRepository
-    ):
-        item = await cache_repository.get_key_value_by_key("asd")
+    def test_fail_to_get_key_value_by_key(self, cache_repository: CacheRepository):
+        item = cache_repository.get_key_value_by_key("asd")
 
         assert item is None
